06460d28302d89e1c4070d18187e9228266107f7
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-07-29 20:50:20 +0500

    modified

--- src/components/Player/MainDashboardApp.jsx
+++ src/components/Player/MainDashboardApp.jsx
@@ -16,14 +16,14 @@ const MainDashboardApp = () => {
 <div className="flex ">
     <Sidebar/>
     <div className='grow ml-16 md:ml-64  bg-gray-100 text-gray-900
-      dark:bg-gray-900 dark:text-white'>
+      dark:bg-gray-900 dark:text-white flex-wrap'>
         <DashboardNavbar/>
         <WelcomeMessage/>
         <div className='mt-9 ml-6 flex flex-wrap'>
           <PlayerProfileCard/> 
           <PlayerProfileDetails/>
         </div>
-        <div>
+        <div className='flex-dr'>
           <PlayerCareerStats/>
         </div>
         <div>
