8aaaba4a97c65763d088771838f1adb72bd6e90b
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-08-05 11:13:10 +0500

    modified

--- src/components/User/Navbar.jsx
+++ src/components/User/Navbar.jsx
@@ -20,21 +20,21 @@ const Navbar = ({ scrollToSection }) => {
 
     return (
         <div className='shadow-md w-full fixed top-0 left-0 z-50'>
-            <div className='md:px-10 py-4 px-7 md:flex justify-between items-center' style={{ backgroundColor: '#00A09A' }}>
+            <div className='md:px-10 py-4 px-7 md:flex justify-between items-center opacity-90' style={{ backgroundColor: '#00A09A' }}>
                 {/* logo here */}
                 <div className='flex text-2xl cursor-pointer items-center gap-2'>
-                    <span className='font-bold text-yellow-300'> SmartCricket</span>
+                    <span className='text-white font-bold '>Smart<span className=' text-yellow-300'>Cricket</span></span>
                 </div>
 
                 {/* menu icons */}
-                <div onClick={() => setIsOpen(!isOpen)} className='w-7 h-7 cursor-pointer absolute right-8 top-6 md:hidden'>
-                    {isOpen ? <FaTimes /> : <FaBars />}
+                <div onClick={() => setIsOpen(!isOpen)} className='w-12 h-12 text-white cursor-pointer absolute right-8 top-6 md:hidden duration-300 '>
+                    {isOpen ? <FaTimes size={20} /> : <FaBars size={20} />}
                 </div>
 
                 {/* nav links here */}
                 <ul className={`md:flex pl-9 md:pl-0 md:items-center justify-center md:static md:pb-0 pb-12 absolute md:z-auto z-[-1] left-0 w-full md:w-auto transition-all duration-500 text-white ease-in ${isOpen ? 'top-12' : 'top-[-490px]'}`} style={{ backgroundColor: '#00A09A' }}>
                     {Links.map((link, index) => (
-                        <li className='font-semibold my-7 md:my-0 md:ml-8 relative' key={index}>
+                        <li className='font-semibold my-7 md:my-0 md:ml-8 relative hover:text-yellow-300 duration-500  ease-out' key={index}>
                             {link.name === 'Ranking' ? (
                                 <div className='relative flex items-center' onMouseEnter={toggleRankingHover} onMouseLeave={toggleRankingHover}>
                                     <Link to={link.link} className='flex items-center'>
@@ -59,7 +59,7 @@ const Navbar = ({ scrollToSection }) => {
                         </li>
                     ))}
                     <li className='font-semibold my-7 md:my-0 md:ml-8'>
-                        <Link to='/signup' className='text-white bg-yellow-400 hover:bg-yellow-300 py-3 px-3 rounded-lg'>Sign Up</Link>
+                        <Link to='/signup' className='text-white bg-yellow-400 hover:bg-yellow-500 py-3 px-3 rounded-md'>Sign Up</Link>
                     </li>
                 </ul>
             </div>
