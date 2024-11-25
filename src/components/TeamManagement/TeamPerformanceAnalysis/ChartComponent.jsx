7a76e8fedd461be0d5c89a9a2fa13e11f47bc38f
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-11-26 01:53:19 +0500

    modified

--- src/components/TeamManagement/TeamPerformanceAnalysis/ChartComponent.jsx
+++ src/components/TeamManagement/TeamPerformanceAnalysis/ChartComponent.jsx
@@ -1,20 +1,17 @@
-// ChartComponent.jsx
 import React from 'react';
 import { Line } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
+const ChartComponent = ({ data, options, title }) => {
+  return (
+    <div className="chart-container">
+      <div className="chart-item">
+        <h3 className="text-center font-semibold text-xl mb-4">{title}</h3>
+        <Line data={data} options={options} />
+      </div>
 
-const ChartComponent = ({ data, options }) => {
-  return <Line data={data} options={options} />;
+
+    </div>
+  );
 };
 
 export default ChartComponent;
