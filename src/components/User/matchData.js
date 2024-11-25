b328ceb5d00d6e3e9dbc4451664ffc53079fa517
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-11-26 01:53:28 +0500

    modified

---
+++ src/components/User/matchData.js
@@ -0,0 +1,18 @@
+export const matchData = {
+    title: "Hong Kong, China T20 Tri-Series, 2024",
+    matchDate: "Tuesday, 12th March 2024, 11:00",
+    venue: "Tin Kwong Road Recreation Ground",
+    teams: [
+        {
+            name: "Pakistan",
+            flag: "/src/components/images/pak.png", // Replace with actual flag image path
+            score: "339-5 (50)",
+        },
+        {
+            name: "India",
+            flag: "/src/components/images/india.png", // Replace with actual flag image path
+            score: "158-10 (41.2)",
+        },
+    ],
+    result: "Pakistan beat India by 180 runs",
+};
