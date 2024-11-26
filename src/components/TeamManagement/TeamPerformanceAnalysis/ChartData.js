ae6a1dd3907b8040dfbb0c005c999c0394153f58
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-11-26 16:34:20 +0500

    modified

--- src/components/TeamManagement/TeamPerformanceAnalysis/ChartData.js
+++ src/components/TeamManagement/TeamPerformanceAnalysis/ChartData.js
@@ -1,22 +1,27 @@
 export const battingAverageData = {
   labels: [
-    'Babar Azam',
-    'Fakhar Zaman',
-    'Imam-ul-Haq',
-    'Mohammad Rizwan',
-    'Shoaib Malik',
+    'Babar Azam (PAK)',
+    'Fakhar Zaman (PAK)',
+    'Imam-ul-Haq (PAK)',
+    'Mohammad Rizwan (PAK)',
+    'Shoaib Malik (PAK)',
+    'Virat Kohli (IND)',
+    'Rohit Sharma (IND)',
+    'KL Rahul (IND)',
+    'Shreyas Iyer (IND)',
+    'Hardik Pandya (IND)',
   ],
   datasets: [
     {
       label: 'Pakistan Batsmen Runs',
-      data: [112, 75, 34, 45, 22], // Pakistan batsmen runs
+      data: [112, 75, 34, 45, 22, null, null, null, null, null], // Pakistan batsmen runs
       borderColor: 'rgba(75, 192, 192, 1)',
       backgroundColor: 'rgba(75, 192, 192, 0.2)',
       fill: true,
     },
     {
       label: 'India Batsmen Runs',
-      data: [45, 30, 25, 40, 10], // India batsmen runs
+      data: [null, null, null, null, null, 90, 85, 50, 60, 30], // India batsmen runs
       borderColor: 'rgba(153, 102, 255, 1)',
       backgroundColor: 'rgba(153, 102, 255, 0.2)',
       fill: true,
@@ -26,24 +31,29 @@ export const battingAverageData = {
 
 export const bowlingPerformanceData = {
   labels: [
-    'Shaheen Afridi',
-    'Mohammad Amir',
-    'Shadab Khan',
-    'Hasan Ali',
-    'Imad Wasim',
-    'Shoaib Malik',
+    'Shaheen Afridi (PAK)',
+    'Mohammad Amir (PAK)',
+    'Shadab Khan (PAK)',
+    'Hasan Ali (PAK)',
+    'Imad Wasim (PAK)',
+    'Shoaib Malik (PAK)',
+    'Jasprit Bumrah (IND)',
+    'Mohammed Shami (IND)',
+    'Ravindra Jadeja (IND)',
+    'Kuldeep Yadav (IND)',
+    'Hardik Pandya (IND)',
   ],
   datasets: [
     {
       label: 'Pakistan Bowlers Wickets',
-      data: [2, 1, 0, 1, 0, 0], // Pakistan bowlers wickets
+      data: [2, 1, 0, 1, 0, 0, null, null, null, null, null], // Pakistan bowlers wickets
       borderColor: 'rgba(255, 99, 132, 1)',
       backgroundColor: 'rgba(255, 99, 132, 0.2)',
       fill: true,
     },
     {
       label: 'India Bowlers Wickets',
-      data: [3, 1, 1, 1, 0, 0], // India bowlers wickets
+      data: [null, null, null, null, null, null, 3, 2, 1, 1, 1], // India bowlers wickets
       borderColor: 'rgba(54, 162, 235, 1)',
       backgroundColor: 'rgba(54, 162, 235, 0.2)',
       fill: true,
