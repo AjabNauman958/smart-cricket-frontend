34e882ca455b4fb036569344545b651514b5a7ea
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-12-08 23:13:42 +0500

    modified

--- src/components/User/Signup.jsx
+++ src/components/User/Signup.jsx
@@ -41,7 +41,6 @@ const Signup = () => {
         }, 2000);
     };
 
-
     const slideFromLeft = {
         hidden: { opacity: 0, x: -100 },
         visible: { opacity: 1, x: 0, transition: { duration: 0.8 } },
