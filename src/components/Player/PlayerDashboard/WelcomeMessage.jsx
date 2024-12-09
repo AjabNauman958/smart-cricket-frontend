82340a243e6e205cfa505324606f3e3cb4c70042
Muhammad Uzair Nisar <uzair.nisar425@gmail.comm>
2024-12-09 22:36:43 +0500

    modified

--- src/components/Player/PlayerDashboard/WelcomeMessage.jsx
+++ src/components/Player/PlayerDashboard/WelcomeMessage.jsx
@@ -7,7 +7,7 @@ const WelcomeMessage = () => {
   const userName = 'Shaheen Shah Afridi';
 
   return (
-    <div className="flex flex-col md:flex-row items-center justify-between bg-[#00A09A] text-white p-6 rounded-lg shadow-lg dark:text-white"style={{flexWrap:'wrap'}} >
+    <div className="flex flex-col md:flex-row items-center justify-around bg-[#00A09A] text-white p-6 rounded-lg shadow-lg dark:text-white" style={{ flexWrap: 'wrap' }} >
       {/* Left Div */}
       <div className="flex flex-col items-center md:items-start mb-6 md:mb-0 md:w-1/2 text-center md:text-left">
         <div className="flex items-center justify-center md:justify-start space-x-2 mb-4">
@@ -26,10 +26,10 @@ const WelcomeMessage = () => {
 
       {/* Right Div */}
       <div className="flex-shrink-0 text-center md:text-left">
-        <img 
-          src={welcomeImage} 
-          alt="Profile" 
-          className="w-32 sm:w-40 md:w-48 lg:w-56 h-auto mx-auto md:mx-0" 
+        <img
+          src={welcomeImage}
+          alt="Profile"
+          className="w-32 sm:w-40 md:w-48 lg:w-56 h-auto mx-auto md:mx-0"
         />
       </div>
     </div>
